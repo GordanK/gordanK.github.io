87ae3ffb3ce910923649834b90b7542dd2e2d7fd
SYSTEX\2200780 <gorden19970622@gmail.com>
2022-09-22 14:20:07 +0800

    0922 14:20

---
+++ src/app/hero.ts
@@ -0,0 +1,18 @@
+export interface Hero {
+  id: number;
+  name: string;
+  description: string;
+}
+
+export const heroes = [
+{
+  id:1,
+  name:'x-men',
+  description:'A men with iron claws'
+},
+
+
+
+
+
+]
