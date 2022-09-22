cf2c266a19fe19bce33ee157c999676b11b5561d
SYSTEX\2200780 <gorden19970622@gmail.com>
2022-09-22 17:28:51 +0800

    0922 17:28

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice' , description:''},
-  { id: 13, name: 'Bombasto' , description:''},
-  { id: 14, name: 'Celeritas' , description:''},
-  { id: 15, name: 'Magneta' , description:''},
-  { id: 16, name: 'RubberMan' , description:''},
-  { id: 17, name: 'Dynama' , description:''},
-  { id: 18, name: 'Dr. IQ' , description:''},
-  { id: 19, name: 'Magma' , description:''},
-  { id: 20, name: 'Tornado' , description:''}
+  { id: 12, name: 'phd. Strange' , description:'loop on my hands'},
+  { id: 13, name: 'Love Maker' , description:'u cannot see me '},
+  { id: 14, name: 'Lifepool' , description:'spider men is my friends'},
+  { id: 15, name: 'white panther' , description:'rip'},
+  { id: 16, name: 'Dr.door' , description:'doom is coming'},
+  { id: 17, name: 'Wonder man' , description:'i got chain in my hands'},
+  { id: 18, name: 'professor z' , description:'wheel-chairmen'},
+  { id: 19, name: 'heHulk' , description:'Megan'},
+  { id: 20, name: 'Captain usa' , description:'egg plant'}
 ];
